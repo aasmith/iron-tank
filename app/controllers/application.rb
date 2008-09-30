1fee5c2c5a51b97792848eec0cbf700670840c50
Andrew Smith <andy@tinnedfruit.org>
2008-09-29 19:51:45 -0700

    Add basic app controller.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,4 +12,11 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+  
+  before_filter :load_user
+
+  # TODO: unstub this.
+  def load_user
+    @user = User.find_by_username("andy")
+  end
 end
