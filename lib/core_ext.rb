c07ae395fe8fc88ea5fb488d8ba73060fc7a0d60
Andrew Smith <andy@tinnedfruit.org>
2008-10-04 14:20:27 -0700

    Add Array#cap.

--- lib/core_ext.rb
+++ lib/core_ext.rb
@@ -11,3 +11,13 @@ class Money
   end
 end
 
+class Array
+  # Returns the first n elements, plus another element indicating how many
+  # elements are left in the array.
+  def cap(n)
+    return [] if n.zero?
+    return self unless size > n
+    first(n-1) << self[n-1, size].size
+  end
+end
+
--- test/unit/core_ext_test.rb
+++ test/unit/core_ext_test.rb
@@ -12,4 +12,12 @@ class CoreExtTest < ActiveSupport::TestCase
       assert_match /\$-?[\.0]*#{i.abs}/, Money.new(i).format
     end
   end
+
+  test "Array#cap" do
+    assert_equal ["1", "2", "3", 2], %w(1 2 3 4 5).cap(4)
+    assert_equal %w(1 2), %w(1 2).cap(2)
+    assert_equal %w(1 2), %w(1 2).cap(5)
+    assert_equal %w(), %w().cap(5)
+    assert_equal %w(), %w(1 2).cap(0)
+  end
 end
