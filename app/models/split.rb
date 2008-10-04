7e217fdf02fa33656004b5d557d5d737347a9ad2
Andrew Smith <andy@tinnedfruit.org>
2008-10-04 16:17:26 -0700

    Add named_scope Split#since

--- app/models/split.rb
+++ app/models/split.rb
@@ -2,6 +2,10 @@ class Split < ActiveRecord::Base
   belongs_to :entry
   belongs_to :ledger
 
+  named_scope :since, lambda { |date|
+    { :conditions => ["entries.posted > ?", date], :include => :entry }
+  }
+
   composed_of(:amount, 
     :class_name => "Money", 
     :mapping => %w(amount cents)) {|amt| amt.to_money}
--- test/unit/split_test.rb
+++ test/unit/split_test.rb
@@ -4,4 +4,8 @@ class SplitTest < ActiveSupport::TestCase
   test "fixtures valid" do
     assert Split.all.all?(&:valid?)
   end
+
+  test "since" do
+    assert Split.since(3.days.ago).all?{|s|s.entry.posted > 3.days.ago.to_date}
+  end
 end
