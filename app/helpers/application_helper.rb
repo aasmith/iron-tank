f9aaceef28cc0ff9ebc810ce1dcf13ae93613f57
Andrew Smith <andy@tinnedfruit.org>
2008-10-04 23:24:53 -0700

    Add pretty_date parameter to show/hide month

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,12 @@ module ApplicationHelper
     content_tag("style", css.scan(/^.*?!wk-hack.*?$/).join("\n"))
   end
 
-  def pretty_date(date)
+  def pretty_date(date, show_month = true)
     ordinal_day = date.day.ordinalize
     ordinal_day.sub!(/[[:alpha:]]+/){ |m| content_tag("sup", m) }
 
-    date.strftime("%A, %%s %B") % ordinal_day
+    date_fmt = "%A, %%s"
+    date_fmt << " %B" if show_month
+    date.strftime(date_fmt) % ordinal_day
   end
 end
--- test/unit/application_helper_test.rb
+++ test/unit/application_helper_test.rb
@@ -6,5 +6,8 @@ class ApplicationHelperTest < ActionView::TestCase
   test "pretty_date as html" do
     assert_equal "Thursday, 1<sup>st</sup> May", 
       pretty_date(Date.parse("2008-05-01"))
+
+    assert_equal "Thursday, 1<sup>st</sup>", 
+      pretty_date(Date.parse("2008-05-01"), false)
   end
 end
