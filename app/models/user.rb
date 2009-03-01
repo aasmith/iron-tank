5eb3a0e3c0e553afe65fad2fb95edf3f45491165
Andrew Smith <andy@tinnedfruit.org>
2009-03-01 13:25:15 -0800

    Add User#ledgers_by_alias.

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,29 @@ class User < ActiveRecord::Base
   has_many :mappings, :through => :ledgers
   has_many :splits, :through => :ledgers
   has_many :entries, :order => "posted DESC"
+
+  # Finds ledgers for the current user by checking the user's mappings.
+  def ledgers_by_alias(name)
+    strategies = [
+      lambda { 
+        mapping = mappings.detect { |mapping| mapping.match?(name) }
+        mapping && mapping.ledger
+      },
+      lambda { ledgers.find_by_name(name.titleize) },
+      lambda { ledgers.find_by_name(name) }
+    ]
+
+    # iterate through each strategy and execute, until one returns one or more ledgers.
+    strategies.each do |s| 
+      ledgers = s.call
+      return ledgers if ledgers.present?
+    end
+
+    return nil
+  end
+
+  # Finds or creates a ledger of the given name (titleized).
+  def ledgers_by_alias!(name)
+    ledgers_by_alias(name) || expenses.create!(:name => name.titleize)
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,35 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @user = users(:andy)
+  end
+
+  test "finds existing ledger by mapping" do
+    m = @user.mappings.first
+    assert_equal m.ledger, @user.ledgers_by_alias(m.value)
+  end
+
+  test "finds existing ledger by name" do
+    @user.expenses.create!(:name => name="Foo #{rand(56)}")
+
+    assert_equal name, @user.ledgers_by_alias(name).name
+  end
+
+  test "finds existing ledger by titleized name" do
+    name = "Auto Service"
+
+    @user.expenses.create!(:name => name)
+
+    assert_equal name, @user.ledgers_by_alias(name.upcase).name
+  end
+
+  test "creates new ledger with titleized name" do
+    s = "CAR WASHES"
+
+    assert_nil @user.ledgers_by_alias(s)
+
+    ledger = @user.ledgers_by_alias!(s)
+    assert_equal s.titleize, ledger.name
   end
 end
